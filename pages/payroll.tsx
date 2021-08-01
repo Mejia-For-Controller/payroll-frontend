1fbcc2765ae9c264e85c857ec533c411c5ff527f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-08-01 11:59:48 -0700

    okay try again

--- pages/payroll.tsx
+++ pages/payroll.tsx
@@ -1,15 +1,18 @@
-import TableauEmbed from '../components/tableau'
+//import TableauEmbed from '../components/tableau'
 import TableauEmbedFunc from '../components/tableaufun'
 //import BasicEmbed from '../components/basicembed'
 
 function Payroll() {
   return <div className='height100'>
+    <div suppressHydrationWarning={true}  className='height100'>
     <script key='tableauscript' src='https://public.tableau.com/javascripts/api/tableau-2.min.js'></script>
-     <TableauEmbedFunc
-      key='payroll1'
-      url='https://public.tableau.com/views/LAPayrollFY2020withNames/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link'
-    />
-    </div>
+      {
+        process.browser &&
+        <TableauEmbedFunc
+          key='payroll1'
+          url='https://public.tableau.com/views/LAPayrollFY2020withNames/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link'
+        />}
+    </div></div>
 }
 
 export default Payroll
