75a0ac444e3c497e9149c59ec0f47817fd19e28f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-07-31 23:59:09 -0700

    wtf is going on

--- pages/payroll.tsx
+++ pages/payroll.tsx
@@ -5,18 +5,16 @@ import TableauEmbedFunc from '../components/tableaufun'
 import Head from 'next/head'
 
 function Payroll() {
-  return <div suppressHydrationWarning={true} className='height100'>
+  return <div className='height100'>
     <Head>
     <meta name="twitter:title" content="Los Angeles Employee Payroll 2020 -Employee Names and Salaries"></meta>
 <meta name="twitter:description" content="Search Names and Job Titles and see their Salaries and Benefits."></meta>
 <meta name="twitter:image" content="https://data.mejiaforcontroller.com/cityworkercard.png"></meta>
     </Head>
-    {
-    process.browser && <TableauEmbedFunc
+     <TableauEmbedFunc
       key='payroll1'
       url='https://public.tableau.com/views/LAPayrollFY2020withNames/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link'
     />
-  }
     </div>
 }
 
