49f9ac24ce1941eb0ce813c6a1839403118f932d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-07-31 23:54:23 -0700

    move head to top

--- pages/payroll.tsx
+++ pages/payroll.tsx
@@ -5,17 +5,18 @@ import TableauEmbedFunc from '../components/tableaufun'
 import Head from 'next/head'
 
 function Payroll() {
-  return <div suppressHydrationWarning={true} className='height100'>{
-    process.browser && <TableauEmbedFunc
-      key='payroll1'
-      url='https://public.tableau.com/views/LAPayrollFY2020withNames/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link'
-    />
-  }
+  return <div suppressHydrationWarning={true} className='height100'>
     <Head>
     <meta name="twitter:title" content="Los Angeles Employee Payroll 2020 -Employee Names and Salaries"></meta>
 <meta name="twitter:description" content="Search Names and Job Titles and see their Salaries and Benefits."></meta>
 <meta name="twitter:image" content="https://data.mejiaforcontroller.com/cityworkercard.png"></meta>
     </Head>
+    {
+    process.browser && <TableauEmbedFunc
+      key='payroll1'
+      url='https://public.tableau.com/views/LAPayrollFY2020withNames/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link'
+    />
+  }
     </div>
 }
 
